a4b65606ba4e5c11a0ea77519c50789d7896c3fa
Hayden Kreuter <hayden.kreuter@me.com>
2022-10-25 18:27:42 -0400

    implement view movies and tests

--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -1,5 +1,5 @@
 from src.models.movie import Movie
-
+from typing import List, Union
 _movie_repo = None
 
 
@@ -10,13 +10,13 @@ def get_movie_repository():
         """In memory database which is a simple list of movies"""
 
         def __init__(self) -> None:
-            self._db: list[Movie] = []
+            self._db: List[Movie] = []
 
-        def get_all_movies(self) -> list[Movie]:
+        def get_all_movies(self) -> List[Movie]:
             """Simply return all movies from the in-memory database"""
             return self._db
 
-        def get_movie_by_title(self, title: str) -> Movie | None:
+        def get_movie_by_title(self, title: str) -> Union[Movie,None]:
             """Get a single movie by its title or None if it does not exist"""
             # Perform a linear search through the in-memory database
             for movie in self._db:
@@ -29,6 +29,10 @@ def get_movie_repository():
         def create_movie(self, title: str, director: str, rating: int) -> Movie:
             """Create a new movie and return it"""
             # Create the movie instance
+            if rating < 0:
+                rating = 0 
+            if rating > 5:
+                rating = 5
             movie = Movie(title, director, rating)
             # Save the instance in our in-memory database
             self._db.append(movie)
--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,30 @@
 # TODO: Feature 1
+# TODO: Feature 1
+from src.repositories import movie_repository as movie_r
+from app import app
+def test_list_all_movies_page():
+    test_app = app.test_client()
+    movie_repository = movie_r.get_movie_repository()
+    movie_repository.create_movie("Title1", "director1", 3)
+    movie_repository.create_movie("Title2", "director2", 0)
+    movie_repository.create_movie("Title3", "director3", 5)
+    response = test_app.get('/movies')
+    assert b'<td>Title1</td>' in response.data
+    assert b'<td>Title2</td>' in response.data
+    assert b'<td>Title3</td>' in response.data
+    assert b'<td>director1</td>' in response.data
+    assert b'<td>director2</td>' in response.data
+    assert b'<td>director3</td>' in response.data
+
+def test_list_all_movies_page_rating_bounds():
+    movie_repository = movie_r.get_movie_repository()
+    movie_repository.create_movie("Title1", "director1", -1)
+    movie_repository.create_movie("Title2", "director2", 0)
+    movie_repository.create_movie("Title3", "director3", 3)
+    movie_repository.create_movie("Title4", "director4", 6)
+    for movie in movie_repository.get_all_movies():
+        assert movie.rating >= 0
+        assert movie.rating <= 5
+
+
+
